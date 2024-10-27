c1c45ca21df838671cb5930bd934fd5243b13526
Daegyeom Ha <combbm@gmail.com>
2024-10-27 14:48:14 +0900

    feat: implement main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import type { Component } from 'solid-js';
+import Header from './components/Header';
+import RaidTotal from './components/RaidTotal';
+import RaidFilter from './components/RaidFilter';
+import RaidTable from './components/RaidTable';
+import useRaidData from './hooks/useRaidData';
 
 const App: Component = () => {
+  const { raid, setRaid, level, setLevel, totalGold, totalPlusGold, totalItem, totalPlusItem } = useRaidData();
+
   return (
-    <div class="flex items-center justify-center">
-      <p class="text-lg text-blue-500">A</p>
+    <div class="flex h-screen w-screen justify-center bg-gray-100/60">
+      <div class="h-full w-[500px] bg-white p-[30px]">
+        <Header />
+        <RaidFilter raid={raid} setRaid={setRaid} level={level} setLevel={setLevel} />
+        <div class="mt-10">
+          <RaidTotal
+            totalGold={totalGold()}
+            totalPlusGold={totalPlusGold()}
+            totalItem={totalItem()}
+            totalPlusItem={totalPlusItem()}
+          />
+        </div>
+        <RaidTable raid={raid()} level={level()} />
+      </div>
     </div>
   );
 };
