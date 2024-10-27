d26b2415c92ca75fe45edf0e70d73c141e56f467
Daegyeom Ha <combbm@gmail.com>
2024-10-27 14:58:20 +0900

    fix: fix table width

--- src/components/RaidTable/index.tsx
+++ src/components/RaidTable/index.tsx
@@ -12,16 +12,16 @@ const RaidTable: Component<RaidTableProps> = (props) => {
     <table class="w-full">
       <thead class="h-8">
         <tr>
-          <th class="border-[3px] border-white bg-zinc-800 px-8 py-2 text-white">관문</th>
+          <th class="w-9 border-[3px] border-white bg-zinc-800 px-8 py-2 text-white">관문</th>
           <th class="border-[3px] border-white bg-zinc-800 text-white">재료</th>
-          <th class="border-[3px] border-white bg-zinc-800 text-white">골드</th>
+          <th class="w-40 border-[3px] border-white bg-zinc-800 text-white">골드</th>
         </tr>
       </thead>
       <tbody>
         <For each={Object.entries(RAID_DATA[props.raid]?.[props.level] ?? {})}>
           {([gate, data]) => (
             <tr>
-              <td class="border-[3px] border-white bg-zinc-100 px-5 py-3 text-center">{gate}</td>
+              <td class="w-9 border-[3px] border-white bg-zinc-100 px-5 py-3 text-center">{gate}</td>
               <td class="space-y-1.5 border-[3px] border-white bg-zinc-100 px-5 py-3 text-left leading-4">
                 <For each={data.재료}>
                   {(item, index) => (
@@ -33,7 +33,7 @@ const RaidTable: Component<RaidTableProps> = (props) => {
                   )}
                 </For>
               </td>
-              <td class="border-[3px] border-white bg-zinc-100 px-5 py-3 text-left leading-4">
+              <td class="w-40 border-[3px] border-white bg-zinc-100 px-5 py-3 text-left leading-4">
                 {data.재화}골드 <br />
                 <span class="text-sm">(더보기 {data.재화_더보기}골드)</span>
               </td>
